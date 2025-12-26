877485cb8dbf924f17582b3325ec6a519779db86
kejicjs11 <154772318+kejicjs11@users.noreply.github.com>
2025-12-26 16:35:46 +0100

    Update hostels.js

--- api/hostels.js
+++ api/hostels.js
@@ -1,15 +1,3 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://kejicjs11:keji4265@cluster0.8cdujmv.mongodb.net/abrakahomes?retryWrites=true&w=majority"
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
